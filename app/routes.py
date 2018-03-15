8579024237f71ad4939c3dee4b855b8533c9efb3
MattW216 <mattwalker216@gmail.com>
2018-03-14 23:26:14 -0400

    completed form views

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,19 +18,12 @@ def index():
         {
             'author': {'username': 'Matt'},
             'body': 'Get Out was a really good movie! Irure dolor ipsum occaecat non nulla exercitation. Fugiat mollit esse non nulla cillum irure enim sit occaecat. Laboris commodo minim qui cillum sunt est adipisicing. Veniam duis ipsum cillum nostrud veniam do ad ipsum ex amet ipsum nulla.'
-        },
-        {
-            'author': {'username': 'Matt'},
-            'body': 'Get Out was a really good movie! Irure dolor ipsum occaecat non nulla exercitation. Fugiat mollit esse non nulla cillum irure enim sit occaecat. Laboris commodo minim qui cillum sunt est adipisicing. Veniam duis ipsum cillum nostrud veniam do ad ipsum ex amet ipsum nulla.'
-        },
-        {
-            'author': {'username': 'Matt'},
-            'body': 'Get Out was a really good movie! Irure dolor ipsum occaecat non nulla exercitation. Fugiat mollit esse non nulla cillum irure enim sit occaecat. Laboris commodo minim qui cillum sunt est adipisicing. Veniam duis ipsum cillum nostrud veniam do ad ipsum ex amet ipsum nulla.'
-        },
-        {
-            'author': {'username': 'Matt'},
-            'body': 'Get Out was a really good movie! Irure dolor ipsum occaecat non nulla exercitation. Fugiat mollit esse non nulla cillum irure enim sit occaecat. Laboris commodo minim qui cillum sunt est adipisicing. Veniam duis ipsum cillum nostrud veniam do ad ipsum ex amet ipsum nulla.'
         }
     ]
     return render_template('index.html', title='Microblog', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
     
